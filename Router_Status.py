4fa2ecda9dd2077116c07725a9729d789aca105a
Damien Jeremiah <michelld@hotmail.com>
2016-10-25 13:16:41 +0200

    Basic Router Status check. Up/Down on connection. This will be run before the SSH establishing.

--- Router_Status.py
+++ Router_Status.py
@@ -3,3 +3,17 @@ import time
 import sys
 import os
 
+# Predefined ip address to Open Gear
+HOSTNAME = "152.94.73.138" # IP to Open Gear 1
+
+response = os.system("ping " + HOSTNAME + ' -n ' + '1')
+
+# Check response
+if response == 0:
+  print '\n\nConnection to Open Gear is up! Have fun!\n'
+  time.sleep(2)
+  print 'SSH is ready to establish connection to the Open Gear\n'
+else:
+  sys.exit("\n\nForce Ending Code - **ABORTING**\n"
+           "\nConnection to the Open Gear is down! Please check the Open Gear and the physical connections!"
+           "\n---------------------------------------------------------------------------------------------")
