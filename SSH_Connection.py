7d78b457cb3d87d6f41b066faa5fd68958469c3c
Damien Jeremiah <michelld@hotmail.com>
2016-12-02 14:33:48 +0100

    Configuration file created (Credentials will be imported - not shown).  Manual insert of PORT for Router / Switch.  Basic configuration added - > Banner motd. Time delay in between commands added.   Will import Router_Status of Open Gear 1/ 2 into the connection.

--- SSH_Connection.py
+++ SSH_Connection.py
@@ -1,6 +1,20 @@
+__author__ = 'Damien'
+
 import paramiko
 import time
+import config
 import sys
-import os
 
+PORT = int(input('Type in port number for the Router / Switch\n\n'))
+#PORT = 3033
 
+# SSH Package
+ssh = paramiko.SSHClient()
+ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy()) #Add policy (RSA Crypt key)
+
+ssh.connect('152.94.73.138', port=PORT, username=config.username, password=config.password)
+ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command('\n')
+ssh_stdin.write('enable\nconf term\nno ip domain-lookup\n')
+time.sleep(1)
+ssh_stdin.write('banner motd #Unauthorized access denied#\nexit\n')
+#ssh_stdin.write('conf term\n')
