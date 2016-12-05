1fa76c51fecd99ba371cdc7da2da9cacacdce2b4
Damien Jeremiah <michelld@hotmail.com>
2016-12-05 19:49:23 +0100

    Changed import file for username storage.

--- SSH_Connection.py
+++ SSH_Connection.py
@@ -2,7 +2,7 @@ __author__ = 'Damien'
 
 import paramiko
 import time
-import config
+import credentials
 import sys
 
 PORT = int(input('Type in port number for the Router / Switch\n\n'))
@@ -12,9 +12,8 @@ PORT = int(input('Type in port number for the Router / Switch\n\n'))
 ssh = paramiko.SSHClient()
 ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy()) #Add policy (RSA Crypt key)
 
-ssh.connect('152.94.73.138', port=PORT, username=config.username, password=config.password)
+ssh.connect('152.94.73.138', port=PORT, username=credentials.username, password=credentials.password)
 ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command('\n')
 ssh_stdin.write('enable\nconf term\nno ip domain-lookup\n')
 time.sleep(1)
 ssh_stdin.write('banner motd #Unauthorized access denied#\nexit\n')
-#ssh_stdin.write('conf term\n')
