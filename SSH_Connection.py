fb80f073f50997f40b91a4cf57cdb3f741a59f8d
Damien Jeremiah <michelld@hotmail.com>
2016-12-05 20:43:26 +0100

    Configuration imported from datafolder instead of being inside main.

--- SSH_Connection.py
+++ SSH_Connection.py
@@ -1,9 +1,12 @@
 __author__ = 'Damien'
 
+from Data import configuration
 import paramiko
 import time
 import credentials
 import sys
+
+
 
 PORT = int(input('Type in port number for the Router / Switch\n\n'))
 #PORT = 3033
@@ -14,6 +17,5 @@ ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy()) #Add policy (RSA Crypt
 
 ssh.connect('152.94.73.138', port=PORT, username=credentials.username, password=credentials.password)
 ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command('\n')
-ssh_stdin.write('enable\nconf term\nno ip domain-lookup\n')
-time.sleep(1)
-ssh_stdin.write('banner motd #Unauthorized access denied#\nexit\n')
+
+configuration.router_settings_basic()
