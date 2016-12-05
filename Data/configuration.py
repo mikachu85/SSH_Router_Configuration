de57e88324410cbbcbe9acc98dd8a09e33f0991a
Damien Jeremiah <michelld@hotmail.com>
2016-12-05 20:42:40 +0100

    Changed timer settings. 0.5 Seconds delay is enough for configuration upload.

--- Data/configuration.py
+++ Data/configuration.py
@@ -6,10 +6,13 @@ ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command('\n')
 
 def router_settings_basic():
     ssh_stdin.write('enable\nconf term\nno ip domain-lookup\n')
-    time.sleep(1)
+    time.sleep(0.5)
     ssh_stdin.write('banner motd #Unauthorized access denied#\nexit\n')
-    time.sleep(1)
+    time.sleep(0.5)
     ssh_stdin.write('conf term\nline con 0\nloggin sync\npassword cisco\nlogin\nexit\n')
-    time.sleep(1)
+    time.sleep(0.5)
     ssh_stdin.write('line vty 0 4\npassword cisco\nexit\n')
-    time.sleep(1)
+    time.sleep(0.5)
+    print "Basic configurations uploaded successfully to unit"
+
+
