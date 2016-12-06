e01409c6715fb0b9e305077c05fa51415f565631
Damien Jeremiah <michelld@hotmail.com>
2016-12-06 21:53:30 +0100

    Configuration now includes settings which will be uploaded to the Unit. We will start with Routers and move on to Switches and ASA. Cleaned up SSH_connection.py

--- Data/configuration.py
+++ Data/configuration.py
@@ -1,18 +1,34 @@
 import paramiko
 import time
-
-ssh = paramiko.SSHClient()
-ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command('\n')
+import credentials
 
 def router_settings_basic():
+    PORT = 3033
+    #PORT = int(input('Type in port number for the Router / Switch\n\n'))
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())  # Add policy (RSA Crypt key)
+    print "SSHClient loaded..\n\nPolicy Added..\n"
+    ssh.connect('152.94.73.138', port=PORT, username=credentials.username, password=credentials.password)
+    ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command('\n')
     ssh_stdin.write('enable\nconf term\nno ip domain-lookup\n')
-    time.sleep(0.5)
-    ssh_stdin.write('banner motd #Unauthorized access denied#\nexit\n')
-    time.sleep(0.5)
-    ssh_stdin.write('conf term\nline con 0\nloggin sync\npassword cisco\nlogin\nexit\n')
-    time.sleep(0.5)
-    ssh_stdin.write('line vty 0 4\npassword cisco\nexit\n')
-    time.sleep(0.5)
-    print "Basic configurations uploaded successfully to unit"
+    time.sleep(0.4)
+    ssh_stdin.write('line con 0\npassword cisco\nlogin\nlogging synchronous\nexit\n')
+    time.sleep(0.4)
+    ssh_stdin.write('line vty 0 4\npassword cisco\nlogin\nexit\n')
+    time.sleep(0.2)
+    print "Basic configuration uploaded successfully to unit"
 
+
+# def router_settings_eigrp():
+#     # Warning ---Start from Conf Term---
+    print "SSHClient loaded..\n\nPolicy Added..\n"
+    ssh_stdin.write('router eigrp 1\nnetwork 10.1.1.0 0.0.0.255')
+    time.sleep(0.4)
+    ssh_stdin.write('eigrp log-neighbor-changes\nno auto-summary')
+
+#
+# def router_settings_ospf():
+#
+#
+# def router_settings_vlan():
 
--- SSH_Connection.py
+++ SSH_Connection.py
@@ -1,21 +1,18 @@
 __author__ = 'Damien'
 
 from Data import configuration
+import credentials
 import paramiko
 import time
-import credentials
 import sys
 
-
-
-PORT = int(input('Type in port number for the Router / Switch\n\n'))
-#PORT = 3033
-
 # SSH Package
-ssh = paramiko.SSHClient()
-ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy()) #Add policy (RSA Crypt key)
-
-ssh.connect('152.94.73.138', port=PORT, username=credentials.username, password=credentials.password)
-ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command('\n')
-
 configuration.router_settings_basic()
+
+# configuration.router_settings_vlan()
+
+# configuration.router_settings_eigrp()
+
+# configuration.router_settings_ospf()
+
+
