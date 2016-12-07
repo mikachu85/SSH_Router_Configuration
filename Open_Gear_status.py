8de8b81a1c40e7b772e9cc583030381dc3038565
Damien Jeremiah <michelld@hotmail.com>
2016-12-07 15:27:40 +0100

    Opengear status method - into ssh_connection. Now the check will occur from start.

--- Open_Gear_status.py
+++ Open_Gear_status.py
@@ -5,31 +5,32 @@ import time
 import sys
 import os
 
+def open_gear_stat():
 # Predefined ip address to Open Gear
-INDATA = raw_input('\nType a for Open Gear 1, b for Open Gear 2 or any other key to manually insert IP\n\n')
+    INDATA = raw_input('\nType a for Open Gear 1, b for Open Gear 2 or any other key to manually insert IP\n\n')
 
-if INDATA == 'a':
-    print '\nOpen Gear 1 Selected'
-    HOSTNAME = '152.94.73.138'
-    # print "\n" + HOSTNAME
-elif INDATA == 'b':
-    print 'Open Gear 2 Selected'
-    HOSTNAME = '152.94.73.139'
-    # print "\n" + HOSTNAME
-else:
-    HOSTNAME = raw_input('Please type in a valid IP address to the Open Gear:\n\n')
-    # print "\n" + HOSTNAME
+    if INDATA == 'a':
+        print '\nOpen Gear 1 Selected'
+        HOSTNAME = '152.94.73.138'
+        # print "\n" + HOSTNAME
+    elif INDATA == 'b':
+        print 'Open Gear 2 Selected'
+        HOSTNAME = '152.94.73.139'
+        # print "\n" + HOSTNAME
+    else:
+        HOSTNAME = raw_input('Please type in a valid IP address to the Open Gear:\n\n')
+        # print "\n" + HOSTNAME
 
-time.sleep(2)
+    time.sleep(2)
 
-response = os.system("ping " + HOSTNAME + ' -n ' + '1')
+    response = os.system("ping " + HOSTNAME + ' -n ' + '1')
 
-# Check response
-if response == 0:
-  print '\n\nConnection to Open Gear is up! Have fun!\n'
-  time.sleep(2)
-  print 'SSH is ready to establish connection to the Open Gear\n'
-else:
-  sys.exit("\n\nForce Ending Code - **ABORTING**\n"
+    # Check response
+    if response == 0:
+        print '\n\nConnection to Open Gear is up! Have fun!\n'
+        time.sleep(2)
+        print 'SSH is ready to establish connection to the Open Gear\n'
+    else:
+        sys.exit("\n\nForce Ending Code - **ABORTING**\n"
            "\nConnection to the Open Gear is down! Please check the Open Gear and the physical connections!"
            "\n---------------------------------------------------------------------------------------------")
--- SSH_Connection.py
+++ SSH_Connection.py
@@ -1,13 +1,18 @@
 __author__ = 'Damien'
 
 from Data import configuration
+import Open_Gear_status
 import credentials
 import paramiko
 import time
 import sys
 
+
+# Open Gear Status
+Open_Gear_status.open_gear_stat()
+
 # SSH Package
-configuration.router_settings_basic()
+# configuration.router_settings_basic()
 
 # configuration.router_settings_vlan()
 
